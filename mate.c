f3b4ca87515689dbbd6ef9a5f91435d186af9326
pedroafonso18 <pedroafonsoprogramador@gmail.com>
2025-05-29 19:16:49 -0300

    feat: Added some configuration and dependencies, thanks for mate.h TomasBorquez

---
+++ mate.c
@@ -0,0 +1,21 @@
+#define MATE_IMPLEMENTATION
+#include "mate.h"
+
+i32 main(void) {
+  StartBuild();
+  {
+    Executable executable = CreateExecutable((ExecutableOptions){
+        .output = "main",
+        .flags = "-Wall -g"
+    });
+
+    AddFile(executable, "./src/*.c");
+    AddFile(executable, "../include/*.c");
+
+    InstallExecutable(executable);
+
+    errno_t err = RunCommand(executable.outputPath);
+    Assert(err == SUCCESS, "RunCommand: failed, return from exe should always be SUCCESS"); // Assert that we return SUCCESS == 0
+  }
+  EndBuild();
+}
